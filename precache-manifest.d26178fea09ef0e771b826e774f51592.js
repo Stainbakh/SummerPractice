6b4847807bd471f781f03b071bb65c3a89fbcc5c
Stainbakh <stainbakh@mail.ru>
2022-07-26 00:26:19 +0300

    Updates

---
+++ precache-manifest.d26178fea09ef0e771b826e774f51592.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "462f7b928c9e8235cdea",
+    "url": "/SummerPractice/static/js/main.462f7b92.chunk.js"
+  },
+  {
+    "revision": "9d7a6bb4a6bd7c3e636d",
+    "url": "/SummerPractice/static/js/1.9d7a6bb4.chunk.js"
+  },
+  {
+    "revision": "17303b84c6552075460d",
+    "url": "/SummerPractice/static/js/2.17303b84.chunk.js"
+  },
+  {
+    "revision": "9e84e40458b5e2f0e0bb",
+    "url": "/SummerPractice/static/js/runtime~main.9e84e404.js"
+  },
+  {
+    "revision": "51861288878252a611305528817e0caa",
+    "url": "/SummerPractice/static/media/MyPhoto.51861288.jpg"
+  },
+  {
+    "revision": "25a1ed3051e2a2d0efb5d3e5f1fc0020",
+    "url": "/SummerPractice/index.html"
+  }
+];
